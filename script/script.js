62499194ce5476507bf519c3e2f1f19b0dc5e6a2
GeritoJavita <geronimo.garciaalarcon@gmail.com>
2025-09-13 09:13:53 -0500

    arreglar codigo espaguetti en css

--- script/script.js
+++ script/script.js
@@ -1,21 +1,42 @@
-// Smooth scrolling for navigation links
-    $(document).ready(function(){
-        $('a.nav-link').on('click', function(event) {
-            if (this.hash !== "") {
-                event.preventDefault();
-                var hash = this.hash;
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top - 70
-                }, 300);
-            }
-        });
-        
-        // Navbar background change on scroll
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > 50) {
-                $('.navbar').addClass('navbar-scrolled');
-            } else {
-                $('.navbar').removeClass('navbar-scrolled');
+
+$(document).ready(function(){
+    // Initialize AOS animation library
+    AOS.init({
+        duration: 800,
+        easing: 'ease-in-out',
+        once: true
+    });
+    
+    // Smooth scrolling for navigation links
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 70
+            }, 300);
+        }
+    });
+    
+    // Navbar background change on scroll
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+            $('.navbar').addClass('navbar-scrolled');
+        } else {
+            $('.navbar').removeClass('navbar-scrolled');
+        }
+    });
+    
+    // Counter animation for stats
+    $('.stat-number').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).data('count')
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
             }
         });
     });
+});
